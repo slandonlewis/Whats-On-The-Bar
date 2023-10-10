57381e7d56e52aa173ad074d6bf19f070456b64f
Seth Lewis <slandonlewis@gmail.com>
2023-10-10 15:48:13 -0500

    added plate display

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ let amountEl = document.querySelector('#amount-el')
 let barWeight = 45
 let totalWeight = barWeight
 let unitSetting = 'LBS'
-// let previousPlate = ''
 let plateArray = []
+let plateImages = []
+let plateDisplayLeft = document.querySelector('#plate-display-left')
+let plateDisplayRight = document.querySelector('#plate-display-right')
 amountEl.textContent = `${totalWeight} ${unitSetting}`
 
 // switch conversion between lbs and kgs
@@ -33,10 +35,18 @@ let changeUnit = (unitValue) => {
 
 // take value of selected weight and add two plates to each side of bar
 let increaseLoad = (plateWeight) => {
-    let addedValue = plateWeight * 2
-    totalWeight += addedValue
-    amountEl.textContent = `${totalWeight} ${unitSetting}`
-    plateArray.push(plateWeight)
+    if ( totalWeight >= 1500 ) {
+        alert('Nice try, bro. But that bar is definitely bent by now...')
+    } else {
+        let addedValue = plateWeight * 2
+        totalWeight += addedValue
+        amountEl.textContent = `${totalWeight} ${unitSetting}`
+        plateArray.push(plateWeight)
+        // EXPERIMENTAL: Add image of plate on screen
+        plateImages.push(`<img src='./${plateWeight}.png'</img>`)
+        plateDisplayLeft.innerHTML = plateImages.join('')
+        plateDisplayRight.innerHTML = plateImages.join('')
+    }
 }
 
 // remove most recent plate added
@@ -49,5 +59,9 @@ let decreaseLoad = () => {
         amountEl.textContent = `${totalWeight} ${unitSetting}`
         plateArray.pop()
         console.log(plateArray)
+        // EXPERIMENTAL: Remove image of plate on screen
+        plateImages.pop()
+        plateDisplayLeft.innerHTML = plateImages.join('')
+        plateDisplayRight.innerHTML = plateImages.join('')
     }
 }
